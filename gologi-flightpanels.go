6b5ca14c8c1e945ebc1847ba6c9bc902421cc2c2
Bjorn Andersson <bjorn@iki.fi>
2018-12-25 00:33:52 +0200

    Progress

--- gologi-flightpanels.go
+++ gologi-flightpanels.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
+//	"fmt"
         "log"
-	"time"
+//	"time"
 	"math"
 	"errors"
 
@@ -11,33 +11,112 @@ import (
 //        "github.com/google/gousb/usbid"
 )
 
+type Switch uint
 const (
-	COM1_1 = 0x01
-	COM2_1 = 0x02
-	NAV1_1 = 0x04
-	NAV2_1 = 0x08
-	ADF_1 = 0x10
-	DME_1 = 0x20
-	XPDR_1 = 0x40
-	COM1_2 = 0x80
+	COM1_1 Switch = iota
+	COM2_1
+	NAV1_1
+	NAV2_1
+	ADF_1
+	DME_1
+	XPDR_1
+	COM1_2
+	COM2_2
+	NAV1_2
+	NAV2_2
+	ADF_2
+	DME_2
+	XPDR_2
+	ACT_1
+	ACT_2
+	ENC1_CW_1
+	ENC1_CCW_1
+	ENC2_CW_1
+	ENC2_CCW_1
+	ENC1_CW_2
+	ENC1_CCW_2
+	ENC2_CW_2
+	ENC2_CCW_2
 )
 
+type Display int
+const (
+	ACTIVE_1 Display = iota
+	STANDBY_1
+	ACTIVE_2
+	STANDBY_2
+)
+
+
 type RadioPanel struct {
-	Device *gousb.Device
-	SwitchState [3]byte
-	DisplayState [20]byte
+	ctx *gousb.Context
+	device *gousb.Device
+	intf *gousb.Interface
+	intfDone func()
+	inEndpoint  *gousb.InEndpoint
+	displayState [20]byte
+	autoUpdate bool
 }
 
-func (self *RadioPanel) DisplayInteger(display int, n int) error {
+func NewRadioPanel() (*RadioPanel, error) {
+	var err error
+	panel := RadioPanel{}
+	for i := 0; i < 20; i++ {
+		panel.displayState[i] = 0x0f
+	}
+
+	panel.ctx = gousb.NewContext()
+	panel.device, err = panel.ctx.OpenDeviceWithVIDPID(0x06a3, 0x0d05)
+	if err != nil {
+		panel.Close()
+		return nil, err
+	}
+	panel.device.SetAutoDetach(true)
+
+	// FIX: implement automatic periodic update
+	panel.autoUpdate = false
+
+	// Initialize switches
+	panel.intf, panel.intfDone, err = panel.device.DefaultInterface()
+	if err != nil {
+		panel.Close()
+		return nil, err
+	}
+
+	panel.inEndpoint, err = panel.intf.InEndpoint(1)
+        if err != nil {
+		panel.Close()
+		return nil, err
+	}
+
+	panel.UpdateDisplay()
+	return &panel, nil
+}
+
+func (self *RadioPanel) Close() {
+	if self.intfDone != nil {
+		self.intfDone()
+	}
+	if self.device != nil {
+		self.device.Close()
+	}
+	if self.ctx != nil {
+		self.ctx.Close()
+	}
+}
+
+func (self *RadioPanel) DisplayInt(display Display, n int) error {
 	return self.DisplayFloat(display, float32(n), 0)
 }
 
-func (self *RadioPanel) DisplayFloat(display int, n float32, decimals int) error {
+func (self *RadioPanel) DisplayFloat(display Display, n float32, decimals int) error {
 	neg := false
 
 	if decimals < 0 || decimals > 5 {
 		return errors.New("decimals out of range")
 	}
+	// Get an integer number that contains all digits
+	// we want to display
 	tempN := int(n * float32(math.Pow10(decimals)))
 	if tempN < 0 {
 		tempN  = -tempN
@@ -52,7 +131,9 @@ func (self *RadioPanel) DisplayFloat(display int, n float32, decimals int) error
 
 	for digit := 0; digit < 5; digit++ {
 		var v int
+		// Get the number we want to display in the 10s
 		pow := int(math.Pow10(digit))
+		// FIX: Show leading zero
 		if pow > tempN {
 			if neg {
 				v = 0xef
@@ -66,31 +147,37 @@ func (self *RadioPanel) DisplayFloat(display int, n float32, decimals int) error
 				v |= 0xd0
 			}
 		}
-		i := display * 5 + 4 -  digit
-		self.DisplayState[i] = byte(v)
+		i := int(display) * 5 + 4 -  digit
+		self.displayState[i] = byte(v)
 	}
-	self.UpdateDisplay()
+	if self.autoUpdate {
+		self.UpdateDisplay()
+	}
 	return nil
 }
 
 func (self *RadioPanel) UpdateDisplay() {
-	//fmt.Printf("%20x ", self.DisplayState)
-	self.Device.Control(0x21, 0x09, 0x03, 0x00, self.DisplayState[0:20])
-}
-
-func NewRadioPanel(dev *gousb.Device) *RadioPanel {
-	panel := RadioPanel{}
-	for i := 0; i < 20; i++ {
-		panel.DisplayState[i] = 0x0f
+	n, err := self.device.Control(0x21, 0x09, 0x03, 0x00, self.displayState[0:20])
+	log.Printf("Wrote %d bytes", n)
+	if err != nil {
+		log.Printf("%v", err)
 	}
-	panel.Device = dev
-	panel.UpdateDisplay()
-	return &panel
+}
+type SwitchState struct {
+	Switch Switch
+	Value uint
 }
 
-func readStream(ep *gousb.InEndpoint, c chan int) {
+func (self *RadioPanel) WatchSwitches() chan SwitchState {
+	c := make(chan SwitchState)
+	go readSwitches(self.inEndpoint, c)
+	return c
+}
+
+func readSwitches(ep *gousb.InEndpoint, c chan SwitchState) {
 	var data [3]byte
-	var buf []byte = data[0:3]
+	var state uint64
+	var newState uint64
 
 	stream, err := ep.NewStream(3, 1)
 	if err != nil {
@@ -99,76 +186,48 @@ func readStream(ep *gousb.InEndpoint, c chan int) {
 	defer stream.Close()
 
 	for {
-		_, err := stream.Read(buf)
+		_, err := stream.Read(data[:])
 		if err != nil {
 			log.Fatalf("Read error: %v", err)
 		}
-		fmt.Printf("%x\n", buf)
-		c <- 1
-	}
-}
-
-func read(ep *gousb.InEndpoint) {
-	var data [3]byte
-	var buf []byte = data[0:3]
-	for {
-		_, err := ep.Read(buf)
-		if err != nil {
-			log.Fatalf("Read error: %v", err)
+		newState = uint64(data[0]) | uint64(data[1]) << 8 |  uint64(data[2]) << 16
+		changed := state  ^ newState
+		state = newState
+		for i := COM1_1; i <= ENC2_CCW_2; i++ {
+			if (changed >> i) & 1 == 1 {
+				val := uint(state >> i & 1)
+				if val == 0 && i != ACT_1 && i != ACT_2 {
+					continue
+				}
+				c <- SwitchState{i, val}
+			}
 		}
-		fmt.Printf("%x\n", buf)
 	}
-}
-
-func write(dev *gousb.Device, c chan int) {
-	data :=  []byte{8, 8, 1, 8, 8, 0xff, 0xff, 0xff, 0xff , 0xff , 0xff, 0xff, 0xff, 0xff, 0xff ,0xff, 0xff, 0xff, 0xff, 0xff  }
-	for {
-	for i := 0; i < 255; i++ {
-		chandata := <-c
-		data[0] = byte(i)
-		fmt.Printf("%d\n", chandata)
-		dev.Control(0x21, 0x09, 0x03, 0x00, data)
-		time.Sleep(100* time.Millisecond)
-	}
-	}
-
 }
 
 func main() {
-	ctx := gousb.NewContext()
-	defer ctx.Close()
-
-	dev, err := ctx.OpenDeviceWithVIDPID(0x06a3, 0x0d05)
+	radioPanel, err := NewRadioPanel()
 	if err != nil {
-		log.Fatalf("Could not open a device: %v", err)
+		log.Fatalf("%v", err)
 	}
-	defer dev.Close()
-	dev.SetAutoDetach(true)
-//	intf, done, err := dev.DefaultInterface()
-//	if err != nil {
-//		log.Fatalf("%s.DefaultInterface(): %v", dev, err)
-//	}
-//	defer done()
-
-	//ep, err := intf.InEndpoint(1)
-	//if err != nil {
-//		log.Fatalf("%s.inEndpoint(): %v", intf, err)
-//	}
-	radioPanel := NewRadioPanel(dev)
-	time.Sleep(10 * time.Millisecond)
-	for i := -9999; i < 9999; i++ {
-		radioPanel.DisplayInteger(0, i)
+	defer radioPanel.Close()
+	c := radioPanel.WatchSwitches()
+	for {
+		switchState := <-c
+		log.Printf("%d: %d", switchState.Switch, switchState.Value)
+		radioPanel.DisplayInt(ACTIVE_1, int(switchState.Switch))
+		radioPanel.DisplayInt(STANDBY_1, int(switchState.Value))
+		radioPanel.UpdateDisplay()
 	}
-
-	//for i := 0; i < 99999; i++ {
-//		radioPanel.SetDisplay(0, float32(99999-i), 0)
-//		radioPanel.SetDisplay(1, float32(i), 0)
-	//}
-	//c1 := make(chan int)
-	//go write(dev, c1)
-	//go readStream(ep, c1)
-	//c2 := make(chan int)
-	//d := <-c2
-	//fmt.Println(d)
-	//time.Sleep(10 * time.Second)
+//	time.Sleep(10 * time.Millisecond)
+//	radioPanel.DisplayFloat(0, 0.1, 2)
+//	radioPanel.UpdateDisplay()
+//	for {
+//		t := time.Now()
+//		radioPanel.DisplayInteger(0, t.Hour())
+//		radioPanel.DisplayInteger(1, t.Minute())
+//		radioPanel.DisplayInteger(3, t.Second())
+//		radioPanel.UpdateDisplay()
+//		time.Sleep(1 * time.Second)
+//	}
 }
